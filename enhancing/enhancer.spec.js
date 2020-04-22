989f3629a13bafa13751f69954e7a0df51b0efc3
Devanee Reid <devaneereid@gmail.com>
2020-04-22 12:48:35 -0600

    initial test all of enhancer.js passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,10 @@
 const enhancer = require('./enhancer.js');
 // test away!
+const { repair, succeed, fail, get } = require('./enhancer.js');
+
+describe('enhancer.js', () => {
+    // Tests All
+    it('should run all tests without errors', () => {
+        expect(true).toBeTruthy();
+    });
+});
